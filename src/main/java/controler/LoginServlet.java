4b0da0951a57bde3ee1fe601b4ceb5772694ecb7
602-08 <602-08@DESKTOP-S0OU2F1>
2023-08-23 13:04:35 +0900

    처음올린파일

---
+++ src/main/java/controler/LoginServlet.java
@@ -0,0 +1,72 @@
+package controler;
+
+import java.io.IOException;
+
+import javax.naming.spi.DirStateFactory.Result;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import book.BookDAO;
+import member.MemberDAO;
+import member.MemberVO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//페이지이동
+		RequestDispatcher rd=request.getRequestDispatcher("/member/login.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//아이디 패스워드 인코딩(x) 한글x utf-8 (x)
+		//파라메타 받기 
+		String id=request.getParameter("userid");
+		String pwd=request.getParameter("pwd");
+		//dao객체생성
+		MemberDAO dao=MemberDAO.getInstance();
+		//매소드 수행
+		MemberVO mvo=dao.login(id, pwd);
+		//결과받기 memoVO//로그인실패
+			//
+		if(mvo!=null) {	
+			HttpSession session=request.getSession();
+			session.setAttribute("mvo", mvo);
+			//페이지에서 필요로 하는 정보를 저장 ??
+			//페이지 이동 : list 쪽으로 이동
+			RequestDispatcher rd=request.getRequestDispatcher("bList");
+			rd.forward(request, response);
+		}else{
+			request.setAttribute("message", "로그인 실패");
+			RequestDispatcher rd=request.getRequestDispatcher("/member/");
+			rd.forward(request, response);
+		}
+		//로그인하게되면 세션저장
+	
+	}
+
+}
